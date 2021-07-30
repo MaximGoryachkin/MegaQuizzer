b64349ec6a2f8c827aaf7b00278c1c4aee867ee3
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-07-30 22:16:26 +0300

    added GUI

---
+++ MegaQuizzer/ViewControllers/CreatingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreatingViewController.swift
+//  MegaQuizzer
+//
+//  Created by slava on 30.07.2021.
+//
+
+import UIKit
+
+class CreatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
